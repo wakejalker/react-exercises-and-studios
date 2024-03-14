c0f2fb225f1247c1b7687efcf3e54825b36970ba
jake <32205542+wakejalker@users.noreply.github.com>
2024-03-14 15:45:46 -0500

    React Part 1 Exercises

--- part1/exercises/src/components/BookList.jsx
+++ part1/exercises/src/components/BookList.jsx
@@ -1,15 +1,15 @@
 export default function BookList() {
-   let pageTitle = "shrug";
-   let book1 = "google.com";
-   let book2 = "";
-   let book3 = "";
+   let pageTitle = "Cookbooks I Own";
+   let book1 = "https://target.scene7.com/is/image/Target/GUEST_1cf444f1-4c70-493a-ba09-3437292e69b5?wid=488&hei=488&fmt=pjpeg";
+   let book2 = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTGadnzGQvQuu3rWUZja_WpAhtlTC95FWmQ918hbs8E49ypGDOSLJk-CriTg_aV_ztidw0&usqp=CAU";
+   let book3 = "https://i5.walmartimages.com/seo/F-ck-That-s-Delicious-An-Annotated-Guide-to-Eating-Well-Hardcover_e257e766-051e-465a-abd0-fc715aa91105.77293ffb5b57a084592abd731bfbb724.jpeg?odnHeight=717&odnWidth=717&odnBg=FFFFFF";
 
    return (
       <div>
          <h3>{pageTitle}</h3>
-         <img src={book1} alt="Nothing here!" />
-         <img src={book2} alt="Nothing here!" />
-         <img src={book3} alt="Nothing here!" />
+         <img src={book1} alt="Chrissy Teigen's second cookbook, Hungry For More" />
+         <img src={book2} alt="The Flavor Bible" />
+         <img src={book3} alt="Cookbook/memoir combo, F*ck, That's Delicious: An Annotated Guide to Eating Well" />
       </div>      
    );
 }
---
+++ part1/exercises/src/components/HobbyLinks.jsx
@@ -0,0 +1,11 @@
+export default function HobbyLinks() {
+    let hobbyLinks = ["https://www.drummerworld.com/", "https://www.youtube.com/"];
+    
+    return (
+        <div>
+        <a href = {hobbyLinks[0]}>Drummer World</a> 
+        <a href = {hobbyLinks[1]}>YouTube</a> 
+        </div>
+     );
+
+}
