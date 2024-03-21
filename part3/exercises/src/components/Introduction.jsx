bb5c2f119a6a96052167b626bc90e65feb8a6f7d
jake <32205542+wakejalker@users.noreply.github.com>
2024-03-21 16:35:58 -0500

    Chapter 31 Exercises

--- part3/exercises/src/components/Introduction.jsx
+++ part3/exercises/src/components/Introduction.jsx
@@ -1,11 +1,11 @@
 export default function HobbyIntroduction () {
    return(
       <div>
-         <h2>3 Fun Facts About Needlepoint</h2>
+         <h2>3 Fun Facts About Drumming</h2>
          <ol>
-            <li>Needlepoint uses a specialized canvas as it's base.</li>
-            <li>The most basic stitch is called a tent stitch.</li>
-            <li>Needlepoint uses a lot of different kinds of fibers.</li>
+            <li>Drumming improves your mood and can be a great stress reliever! While playing you release endorphins.</li>
+            <li>The coordination required for drumming can lead to improved motor skills and coordination in other areas of life.</li>
+            <li>Drumming is oftten used in music therapy to help people express themselves.</li>
          </ol>
       </div>
    );
